c23ee1244e705e13ce77ae14ee1f552852c27a58
eselse <eselse@MacBook-Pro-eselse.local>
2018-08-24 22:12:33 +0300

    first part is done

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".fixed-action-btn");
+  var instances = M.FloatingActionButton.init(elems);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".dropdown-trigger");
+  var instances = M.Dropdown.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
